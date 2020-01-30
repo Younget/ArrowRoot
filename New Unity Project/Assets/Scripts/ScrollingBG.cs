0af1d2c53e7960a1b09ec5937fd0ac7f5f07f843
isaiah <isaiah.mackey@digiPen.com>
2020-01-30 08:09:53 -0800

    oooo Signed-off-by: isaiah <isaiah.mackey@digiPen.com>

---
+++ New Unity Project/Assets/Scripts/ScrollingBG.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBG : MonoBehaviour
+{
+    Vector3 bgPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xPos = GetComponent<Transform>().position.x;
+        if (xPos <= -30.0f)
+            xPos = 30.0f;
+        else
+            xPos -= .1f;
+        bgPosition.x = xPos;
+        gameObject.transform.position = bgPosition;
+
+    }
+}
