6693472d6e9b835b818046e84e664076678de85d
Younget <48969941+Younget@users.noreply.github.com>
2020-01-31 09:12:30 -0800

    score stuff -ethan

---
+++ Score stuff/Score.cs
@@ -0,0 +1,32 @@
+﻿/* Score
+ * By: Ethan Young
+ * Description: The player's score counts up by 10 per second until the player dies.
+ * Last Updated: 1/31/19
+ * */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public float playerScore;
+    public int highScore = 0;
+    float speedSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set the player Score to zero to start
+        playerScore = 0;
+    }
+   
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //set the speed of the timer
+        speedSet += 1 * Time.deltaTime * 5;
+        //Round the timer to an integer
+        playerScore = Mathf.RoundToInt(speedSet);
+    }
+}
---
+++ Score stuff/ScoreText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void Score()
+    {
+        Text myText = GetComponent<Text>();
+        myText.text = "Score: " + FindObjectOfType<Score>().playerScore.ToString();
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Score();
+    }
+}
